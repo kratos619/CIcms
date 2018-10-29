276858df1c44507032cf2fe714e500a07541a499
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-29 13:12:26 +0530

    Create Subjects controller

---
+++ application/controllers/admin/Subjects.php
@@ -0,0 +1,12 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Subjects extends CI_Controller 
+{
+ 
+    public function index()
+	{
+        die('Subjects');
+	//	$this->load->view('welcome_message');
+	}
+}
