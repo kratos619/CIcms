7d8c4c015f229f59a76fbca89463acee1e663a44
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-29 13:12:38 +0530

    create Users Controller

---
+++ application/controllers/admin/Users.php
@@ -0,0 +1,12 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users extends CI_Controller 
+{
+ 
+    public function index()
+	{
+        die('users');
+	//	$this->load->view('welcome_message');
+	}
+}
