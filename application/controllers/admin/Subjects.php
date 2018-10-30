d1a5f4e46edf17833cf34081b8623fc16137f9dc
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-30 11:54:15 +0530

    create controller to call model and perform operation

--- application/controllers/admin/Subjects.php
+++ application/controllers/admin/Subjects.php
@@ -15,7 +15,32 @@ class Subjects extends CI_Controller
 	{
         //die('pages');
 	//	$this->load->view('welcome_message');
-		$this->template->load('admin','default','subjects/add');
+	//$this->template->load('admin','default','subjects/add');
+
+	$this->form_validation->set_rules('name','Subject Name','required');
+
+	if($this->form_validation->run() === false){
+		$this->template->load('admin','default','subjects/add');	
+	}else{
+		$data = array(
+            'name' => $this->input->post('name')
+        );
+
+		// insert data to db 
+		$this->subjects_model->create_subject();
+	 
+		//load activity
+
+		$activity_data = array(
+            'resource_id' => $this->db->insert_id(),
+			'type' => 'subject',
+			'action' => 'added',
+			'message' => 'subject '. $data['name'] 		
+		);
+		// cal activity model
+		$this->activity_model->add_Activity($activity_data);
+		 redirect('admin/subjects');
+	}
 	}
 
 	    public function edit()
