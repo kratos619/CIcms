1423982b9dde29d869655207cf1d59ccd8434c52
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-30 10:58:34 +0530

    create subject controller to load Users

--- application/controllers/admin/Users.php
+++ application/controllers/admin/Users.php
@@ -3,10 +3,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users extends CI_Controller 
 {
- 
     public function index()
 	{
-        die('users');
+        //die('pages');
 	//	$this->load->view('welcome_message');
+		$this->template->load('admin','default','users/index');
+	}
+
+	    public function add()
+	{
+        //die('pages');
+	//	$this->load->view('welcome_message');
+		$this->template->load('admin','default','users/add');
+	}
+
+	    public function edit()
+	{
+        //die('pages');
+	//	$this->load->view('welcome_message');
+		$this->template->load('admin','default','users/edit');
+	}
+
+	    public function delete()
+	{
+        die('user');
+	//	$this->load->view('welcome_message');
+		//$this->template->load('admin','default','pages/index');
+	}
+
+	public function login(){
+		
 	}
 }
