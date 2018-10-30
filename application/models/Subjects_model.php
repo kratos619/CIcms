235af1569eec6a79de79440937d599de7f28f7e3
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-30 11:56:56 +0530

    subject mdel to add subjects to db and get subject lists

---
+++ application/models/Subjects_model.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Subjects_model extends CI_Model 
+{
+    public function __construct(){
+        $this->load->database();
+    }
+
+    public function get_list(){
+        $query = $this->db->get('subjects');
+        return $query->result();
+    }
+
+    public function create_subject(){
+        $data = array(
+            'name' => $this->input->post('name')
+        );
+
+        return $this->db->insert('subjects',$data);
+
+    }
+
+
+}
+
