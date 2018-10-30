16cab539e9b2fe9868d8ab4355bc289a95055287
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-30 10:58:23 +0530

    create subject controller to load subjects

--- application/controllers/admin/Subjects.php
+++ application/controllers/admin/Subjects.php
@@ -6,7 +6,29 @@ class Subjects extends CI_Controller
  
     public function index()
 	{
-        die('Subjects');
+        //die('pages');
 	//	$this->load->view('welcome_message');
+		$this->template->load('admin','default','subjects/index');
+	}
+
+	    public function add()
+	{
+        //die('pages');
+	//	$this->load->view('welcome_message');
+		$this->template->load('admin','default','subjects/add');
+	}
+
+	    public function edit()
+	{
+        //die('pages');
+	//	$this->load->view('welcome_message');
+		$this->template->load('admin','default','subjects/edit');
+	}
+
+	    public function delete()
+	{
+        die('subjects');
+	//	$this->load->view('welcome_message');
+		//$this->template->load('admin','default','pages/index');
 	}
 }
