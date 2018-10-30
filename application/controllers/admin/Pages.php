a8c8a59dca6b31d323f33cfa0e3229370fdd4b76
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-30 10:57:53 +0530

    create page controller to load template

--- application/controllers/admin/Pages.php
+++ application/controllers/admin/Pages.php
@@ -6,7 +6,29 @@ class Pages extends CI_Controller
  
     public function index()
 	{
+        //die('pages');
+	//	$this->load->view('welcome_message');
+		$this->template->load('admin','default','pages/index');
+	}
+
+	    public function add()
+	{
+        //die('pages');
+	//	$this->load->view('welcome_message');
+		$this->template->load('admin','default','pages/add');
+	}
+
+	    public function edit()
+	{
+        //die('pages');
+	//	$this->load->view('welcome_message');
+		$this->template->load('admin','default','pages/edit');
+	}
+
+	    public function delete()
+	{
         die('pages');
 	//	$this->load->view('welcome_message');
+		//$this->template->load('admin','default','pages/index');
 	}
 }
