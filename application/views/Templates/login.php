e3c85c1c979947150e0ced8d0c834cd047e93f4c
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-12 13:58:43 +0530

    Load Login Template

---
+++ application/views/Templates/login.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Codeinginer CMS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url(); ?>assets/css/starter-template.css" rel="stylesheet">
+  </head>
+
+  <body>
+  <div class="container">
+  <h3 class="page-header text-center">
+    Login
+  </h3>
+  <div class="row">
+    <div class="col-md-5 mx-auto">
+        <?php $this->load->view($main)?>
+    </div>
+  </div>
+  </div>
+  </body>
+</html>
+
