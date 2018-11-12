b80fb83600860eaeb2cde9ec3e0afaa5d3a77ee1
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-12 12:10:44 +0530

    User Model Perform CRUD On Users

---
+++ application/models/Users_model.php
@@ -0,0 +1,40 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users_model extends CI_Model
+{
+    public function get_users()
+    {
+        //$this->db->join('subjects','subjects.id = pages.subject_id');
+       $query = $this->db->get('users');
+        return $query->result();
+
+    }
+
+    public function create_user($data)
+    {
+
+        return $this->db->insert('users',$data);
+
+    }
+
+    public function get_user_by_id($id){
+         $this->db->where('id',$id);
+     //   $this->db->join('subjects','subjects.id = pages.subject_id');
+        $query = $this->db->get('users');
+        return $query->row();  
+    }
+
+    public function update_user($id,$data)
+    {
+        $this->db->where('id',$id);
+        $this->db->update('users',$data);
+       return true;
+    }
+
+    public function delete_user($id){
+        $this->db->where('id',$id);
+        $this->db->delete('users');
+        return true;
+    }
+}
